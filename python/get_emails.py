bc0bb1454f15ea77a37372bc6ab4edf255cb9271
Adam Peacock <apeacock@aqorn.com>
2022-11-15 07:55:21 +0000

    more updates

--- python/get_emails.py
+++ python/get_emails.py
@@ -2,53 +2,94 @@
 
 import imaplib
 import email
-import time
 import json
 import sys
+import os
+from email.header import decode_header
 
 global _EMAILDIR_
 _EMAILDIR_ = sys.argv[1]
 
+def clean(text):
+    # clean text for creating a folder
+    return "".join(c if c.isalnum() else "_" for c in text)
 
-start = time.time()
-try:
-    imap_ssl = imaplib.IMAP4_SSL(host="imap.gmail.com", port=imaplib.IMAP4_SSL_PORT)
-except Exception as e:
-    print("ErrorType : {}, Error : {}".format(type(e).__name__, e))
-    imap_ssl = None
+# try:
+#     imap_ssl = imaplib.IMAP4_SSL(host="imap.gmail.com", port=imaplib.IMAP4_SSL_PORT)
+# except Exception as e:
+#     print("ErrorType : {}, Error : {}".format(type(e).__name__, e))
+#     imap_ssl = None
 
-try:
-    resp_code, response = imap_ssl.login("alawson@aqorn.com","ehawuknmsiozktwn")
-except Exception as e:
-    print("ErrorType : {}, Error : {}".format(type(e).__name__, e))
-    resp_code, response = None, None
+# try:
+#     resp_code, response = imap_ssl.login("alawson@aqorn.com","ehawuknmsiozktwn")
+# except Exception as e:
+#     print("ErrorType : {}, Error : {}".format(type(e).__name__, e))
+#     resp_code, response = None, None
 
-resp_code, directories = imap_ssl.list(pattern=_EMAILDIR_)
-for directory in directories:
-    directory_name = directory.decode().split('"')[-2]
-    directory_name = '"' + directory_name + '"'
-    if directory_name == '"[Gmail]"':
-        continue
-    try:
-        resp_code, mail_count = imap_ssl.select(mailbox=directory_name, readonly=True)
-        count = mail_count[0].decode()
-    except Exception as e:
-        resp_code, mail_count = None, None
+# resp_code, directories = imap_ssl.list(pattern=_EMAILDIR_)
+# directory_name = '"' + _EMAILDIR_ + '"'
 
-resp_code, mail_count = imap_ssl.select(mailbox=_EMAILDIR_, readonly=True)
-resp_code, mail_ids = imap_ssl.search(None, "ALL")
+# try:
+#     resp_code, mail_count = imap_ssl.select(mailbox=directory_name, readonly=True)
+#     count = mail_count[0].decode()
+# except Exception as e:
+#     resp_code, mail_count = None, None
 
-messages = []
-messages.append(count)
-for mail_id in mail_ids[0].decode().split():
-    content = ''
-    resp_code, mail_data = imap_ssl.fetch(mail_id, '(RFC822)') ## Fetch mail data.
-    message = email.message_from_bytes(mail_data[0][1])
-    for part in message.walk():
-        if part.get_content_type() == "text/plain":
-            body = part.get_payload(decode=True)
-    content = mail_id,message.get("From"),message.get("To"),message.get("Bcc"),message.get("Date"),message.get("Subject"),body.decode('utf-8')
-    messages.append(content)
+# resp_code, mail_count = imap_ssl.select(mailbox=_EMAILDIR_, readonly=True)
+# resp_code, mail_ids = imap_ssl.search(None, "ALL")
 
-imap_ssl.close()
-print (json.dumps(messages))
+
+
+
+# create an IMAP4 class with SSL 
+imap = imaplib.IMAP4_SSL('imap.gmail.com')
+# authenticate
+imap.login("alawson@aqorn.com","ehawuknmsiozktwn")
+
+
+status, messages = imap.select(_EMAILDIR_, readonly=True)
+# number of top emails to fetch
+N = 3
+# total number of emails
+messages = int(messages[0])
+
+all_messages = []
+all_messages.append(messages)
+# for mail_id in mail_ids[0].decode().split():
+
+for mail_id in range(messages, 0, -1):
+    # fetch the email message by ID
+    res, msg = imap.fetch(str(mail_id), "(RFC822)")
+    for response in msg:
+        if isinstance(response, tuple):
+            # parse a bytes email into a message object
+            msg = email.message_from_bytes(response[1])
+            # decode the email subject
+            subject, encoding = decode_header(msg["Subject"])[0]
+            if isinstance(subject, bytes):
+                # if it's a bytes, decode to str
+                subject = subject.decode(encoding)
+            # decode email sender
+            From, encoding = decode_header(msg.get("From"))[0]
+            if isinstance(From, bytes):
+                From = From.decode(encoding)
+            # if the email message is multipart
+            if msg.is_multipart():
+                # iterate over email parts
+                for part in msg.walk():
+                    try:
+                        # get the email body
+                        msg_body = part.get_payload(decode=True).decode()
+                    except:
+                        pass
+            else:
+                # extract content type of email
+                content_type = msg.get_content_type()
+                # get the email body
+                msg_body = msg.get_payload(decode=True).decode()
+            content = mail_id,email.utils.parseaddr(msg.get("From"))[0],email.utils.parseaddr(msg.get("From"))[1],msg.get("To"),msg.get("Bcc"),msg.get("Date"),msg.get("Subject"),msg_body
+            all_messages.append(content)
+
+imap.close()
+
+print (json.dumps(all_messages))
--- www/html/shared_functions.php
+++ www/html/shared_functions.php
@@ -4,40 +4,34 @@ ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
 
-function run_($cmd) {
-    $command = escapeshellcmd($cmd);
-    $result = shell_exec($command);
-    return $result;
-}
-
 function email_count($mailbox) {
-    $result = run_('python3 /home/email/ml-email/python/get_email_count.py ' . $mailbox);
+    $result = exec('python3 /home/email/ml-email/python/get_email_count.py ' . $mailbox);
     return trim($result);
 }
 
 function display_email_table($mailbox) {
-    $result = json_decode(run_('python3 /home/email/ml-email/python/get_emails.py ' . $mailbox), true);
-    $emails = array_keys($result);
-    // Count = $result[$emails[0]][0]
+    $result = json_decode(exec('python3 /home/email/ml-email/python/get_emails.py ' . $mailbox), true);
+    $emails = array_keys(array_reverse($result));
     echo <<< EOT
     <div class="table-responsive">
         <table class="table table-bordered" id="dataTable" width="100%" cellspacing="0">
         <thead>
             <tr>
-                <th><input type='checkbox' id='allid' name='allid'></th>
-                <!-- <th>Mail Id</th> -->
+                <th><input type='checkbox' id='allid' onclick='toggle(this)'></th>
                 <th>Date</th>
-                <th>From</th>
+                <th>Name</th>
+                <th>Address</th>
                 <th>Subject</th>
             </tr>
         </thead>
         <tbody>
     EOT;
     for($id = 1; $id < count($result); $id++) {
-        echo "<tr><td><input type='checkbox' id='id" . $result[$emails[$id]][0] . "' name='id1'>";
-        echo "<td>" . $result[$emails[$id]][4];
+        echo "<tr><td><input type='checkbox' id='id" . $result[$emails[$id]][0] . "' name='get_emails'>";
+        echo "<td>" . $result[$emails[$id]][5];
         echo "<td>" . $result[$emails[$id]][1];
-        echo "<td>" . $result[$emails[$id]][5] . "</tr>";
+        echo "<td>" . $result[$emails[$id]][2];
+        echo "<td>" . $result[$emails[$id]][6] . "</tr>";
     }
     echo <<< EOT
             </tbody>
